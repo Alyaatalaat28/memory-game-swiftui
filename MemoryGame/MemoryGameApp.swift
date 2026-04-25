0de919cf4d606dfc2550c897904aeda7dfb08405
Alyaatalaat28 <alyaatalaat205@gmail.com>
2026-04-25 11:54:42 +0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Alyaa on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
